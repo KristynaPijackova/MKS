c4d253e45dfd4040f3683f7518b42c3cf4a2e8ad
Kristyna Pijackova <pijackovakristyna@gmail.com>
2021-10-14 09:59:16 +0200

    [Update]

--- Lab1/Lab1/Debug/Src/main.d
+++ Lab1/Lab1/Debug/Src/main.d
@@ -1,19 +1,19 @@
 Src/main.o: ../Src/main.c \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cm0.h \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cmInstr.h \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cmFunc.h \
- D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cm0.h \
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cmInstr.h \
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cmFunc.h \
+ D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cm0.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cm0.h:
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cmInstr.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cmInstr.h:
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include/core_cmFunc.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include/core_cmFunc.h:
 
-D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
--- Lab1/Lab1/Debug/Src/subdir.mk
+++ Lab1/Lab1/Debug/Src/subdir.mk
@@ -25,5 +25,5 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F030x8 -DSTM32F030R8Tx -DSTM32 -DSTM32F0 -DNUCLEO_F030R8 -c -I../Inc -I"D:/Documents/MMIA/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include" -I"D:/Documents/MMIA/Lab1/Lab1/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F030x8 -DSTM32F030R8Tx -DSTM32 -DSTM32F0 -DNUCLEO_F030R8 -c -I../Inc -I"D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Device/ST/STM32F0xx/Include" -I"D:/Documents/xpijac02/MKS/MKS/Lab1/Lab1/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
